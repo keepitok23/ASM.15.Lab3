5646e481b75b6e5657079c48d472e464a8c3cd21
BaranovMisha <bara-m@yandex.ru>
2015-11-26 21:48:32 +0400

    добавил правильную базу

--- lab3/st03/database03.sql
+++ lab3/st03/database03.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 25 2015 г., 20:41
+-- Время создания: Ноя 26 2015 г., 20:46
 -- Версия сервера: 5.5.45
 -- Версия PHP: 5.3.29
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `st03` (
   `diplom` varchar(200) NOT NULL,
   `dipRuk` varchar(200) NOT NULL,
   PRIMARY KEY (`key`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Дамп данных таблицы `st03`
@@ -40,7 +40,9 @@ CREATE TABLE IF NOT EXISTS `st03` (
 
 INSERT INTO `st03` (`key`, `name`, `diplom`, `dipRuk`) VALUES
 (4, 'sdg', 'sdsg', 'adfad1241234'),
-(6, '123', '135d', 'tadg');
+(6, '123', '135d', 'tadg'),
+(7, 'dsg', 'sfg', 'sfg'),
+(8, '123', '123', '34');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
